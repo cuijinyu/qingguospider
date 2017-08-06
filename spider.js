412c458d73221fbdcc6b36e5e8914a14faa04ca4
崔晋瑜 <2510271615@qq.com>
2017-08-06 23:36:35 +0800

    Add files via upload

--- spider.js
+++ spider.js
@@ -211,7 +211,48 @@ spider.prototype.getClass=function () {
 };
 //获取有效成绩
 spider.prototype.getValueScore=function () {
+    var req=http.request({
+        hostname:'bkjw.sxu.edu.cn',
+        port:'80',
+        path:'/xscj/Stu_MyScore_Drawimg.aspx?x=1&h=2&w=867&xnxq=20161&xn=2016&xq=1&rpt=1&rad=2&zfx=0',
+        method:'GET',
+        headers:{
+            'Accept':'image/webp,image/apng,image/*,*/*;q=0.8',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'zh-CN,zh;q=0.8',
+            'Cache-Control':'no-cache',
+            'Connection':'keep-alive',
+            'Cookie':`${this.theCookie}`,
+            'Host':'bkjw.sxu.edu.cn',
+            'Pragma':'no-cache',
+            'Referer':'http://bkjw.sxu.edu.cn/xscj/Stu_MyScore_rpt.aspx',
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+        }
+    },(res)=>{
+        var imageData='';
+        console.log(res);
+        console.log(`状态码: ${res.statusCode}`);
+        res.setEncoding("binary");
+        res.on("data",(chunk)=>{
+            console.log(chunk.toString());
+            imageData+=chunk;
+        });
+        res.on("end",()=>{
+            fs.writeFile('score.png',imageData,"binary",(err)=>{
+                if(err){
+                    console.log(err);
+                }
+            });
+            console.log("OK SUCCESSFUL");
+        })
+    });
 
+    req.on('err',(err)=>{
+        console.log(err);
+    });
+
+    req.end();
+    console.log("获取成绩数据成功");
 };
 //获取spider对象的属性
 spider.prototype.getAttr=function(name){
